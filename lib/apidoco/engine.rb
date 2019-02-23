dcf86a4d25eadca6298e37f1a1266b0f775dff4f
Ionut Mladin <ionutm87@gmail.com>
2019-02-23 14:57:40 +0200

    Update engine.rb

--- lib/apidoco/engine.rb
+++ lib/apidoco/engine.rb
@@ -1,5 +1,8 @@
 module Apidoco
   class Engine < ::Rails::Engine
     isolate_namespace Apidoco
+    initializer :assets do |config|
+      Rails.application.config.assets.paths << root.join("app", "assets")
+    end
   end
 end
